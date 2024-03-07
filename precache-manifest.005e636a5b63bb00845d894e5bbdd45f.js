aa9b033678091f435c282c0f2480c27214dac937
Emanuele Musto <emanueleugo.musto@gmail.com>
2024-03-07 23:42:36 +0100

    Updates

---
+++ precache-manifest.005e636a5b63bb00845d894e5bbdd45f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "627e8ca0cd57ab32df6f",
+    "url": "/scortea/static/css/main.79b32c72.chunk.css"
+  },
+  {
+    "revision": "627e8ca0cd57ab32df6f",
+    "url": "/scortea/static/js/main.627e8ca0.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/scortea/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ab853f436fb7d626eab9",
+    "url": "/scortea/static/js/runtime~main.ab853f43.js"
+  },
+  {
+    "revision": "fa29c2168273e47609bf7f71583f57c9",
+    "url": "/scortea/index.html"
+  }
+];
